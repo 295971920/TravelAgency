7ee62cfd62dcb9fbf25cd9e415c4821adbe419ce
xiaoshan <295971920@qq.com>
2020-02-29 01:11:27 +0800

    update

---
+++ src/pages/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon" >&#xe624;</div>
+      </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<!-- 1rem = html  font-size = 50px   -->
+<!-- 43px = .86rem   即 86/100 -->
+<style lang="stylus" scoped>
+.header
+  display flex
+  line-height 0.86rem
+  background #00bcd4
+  color #fff
+  .header-left
+    width 0.64rem
+    float left
+    .back-icon
+      text-align center
+      font-size .4rem
+  .header-input
+    flex 1
+    height 0.64rem
+    line-height 0.64rem
+    margin-top 0.12rem
+    margin-left 0.2rem
+    padding-left .2rem
+    background #fff
+    border-radius 0.1rem
+    color #ccc
+  .header-right
+    width 1.24rem
+    float right
+    text-align center
+    .arrow-icon
+    margin-left -0.4rem
+      font-size .24rem
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="class">
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from '../components/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style>
+</style>
