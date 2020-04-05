a8bafaffdac11264dd1bf70a3dc2fff992dd6ca0
xiaoshan <295971920@qq.com>
2020-04-05 11:19:18 +0800

    update detail -banner

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+          <!-- slides -->
+          <swiper-slide v-for="(item, index) in imgs" :key="index">
+            <img class="gallary-img" :src="item" alt="" >
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+          <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+        </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        // 将observe应用于Swiper的父元素。当Swiper的父元素变化时，例如window.resize，Swiper更新。
+        observeParents: true,
+        // 启动动态检查器(OB/观众/观看者)，当改变swiper的样式（例如隐藏/显示）或者修改swiper的子元素时，自动初始化swiper。默认false，不开启，可以使用update()方法更新。
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped="scoped">
+  .container >>> .swiper-container
+    overflow: inherit
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    position fixed
+    height 100%
+    left: 0;
+    right: 0;
+    bottom 0
+    background: #000;
+    .wapper
+      background #FFFFFF
+      width 100%
+      padding-bottom 100%
+      .gallary-img
+        width 100%
+    .swiper-pagination
+      color #fff
+      bottom -1rem
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img src="../../../../static/img/detail/001.png" alt="" class="banner-img" />
+      <div class="banner-info">
+        <div class="banner-title">故宫+八达岭长城+鸟水外景一日游</div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe63b;</span>
+          &nbsp;39
+        </div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['../../../static/img/detail/0001.jpg', '../../../static/img/detail/0001.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped="scoped">
+.banner
+  position relative
+  overflow hidden
+  height 0
+  padding-bottom 55%
+  .banner-img
+    width 100%
+  .banner-info
+    display flex
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    line-height 0.6rem
+    color #FFFFFF
+    /* 图片过渡的一个渐变 */
+    background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0 0.8))
+    .banner-title
+      flex 1
+      font-size .32rem
+      padding 0 .2rem
+    .banner-number
+      height .32rem
+      line-height .32rem
+      margin-top .14rem
+      padding 0 .4rem
+      border-radius .2rem
+      background rgba(0, 0, 0, .8)
+      font-size .24rem
+      .banner-icon
+        font-size .24rem
+</style>
