ecb2587fa96fbb89f1f89f8b4131aba3699606b7
xiaoshan <295971920@qq.com>
2020-04-06 10:08:33 +0800

    update detail list

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index.title">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <!-- 使用递归获取数据-->
+      <div class="item-children" v-if="item.children">
+        <!-- 自己给自己传递数据  -->
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height .8rem
+    font-size .32rem
+    padding 0 .2rem
+    .item-title-icon
+      position relative
+      left .06rem
+      top .2rem
+      line-height .8rem
+      display inline-block
+      width .36rem
+      height .36rem
+      background url(../../../../static/img/detail/detail.png)
+      margin .1rem
+      background-size .4rem 3rem
+  .item-children
+    padding 0 .2rem
+</style>
