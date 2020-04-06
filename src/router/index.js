9223f2a60e0549efb39a9e153bc0cb49f8505a82
xiaoshan <295971920@qq.com>
2020-04-06 17:46:35 +0800

    update Notes

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home'
-import City from '@/pages/city/City'
-import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
 export default new Router({
+  /* component: () => import('路径') 实现异步请求 */
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () => import('@/pages/home/Home')
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      component: () => import('@/pages/city/City')
     },
     {
       /* 绑定id */
       path: '/detail/:id',
       name: 'Detail',
-      component: Detail
+      component: () => import('@/pages/detail/Detail')
     }],
+  /* 重置detail目录下header中handleScroll函数的坐标，解决大坐标引起的白屏 */
   scrollBehavior (to, from, savedPosition) {
     return { x: 0, y: 0 }
   }
